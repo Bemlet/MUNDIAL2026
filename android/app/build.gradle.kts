f6bf4e9154ce8470b89e2671a364ef46e61cb907
ever <evermosquerag@gmail.com>
2026-06-17 12:00:51 -0300

    fix: minSdk 23 (supabase_flutter/ua_client_hints requiere >=22)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
         applicationId = "com.ever.mundial2026"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // supabase_flutter (ua_client_hints) requiere minSdk >= 22.
+        minSdk = maxOf(flutter.minSdkVersion, 23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
